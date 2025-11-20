8ab62119ca85d86cc4b623e2d04f673ab901a7ca
manoj kadali <{your_email}>
2025-11-20 19:48:56 +0530

    e1

---
+++ app.js
@@ -0,0 +1,26 @@
+// nested HTML elements
+{/* <div id="parent">
+    <div id="child">
+        <h1></h1>
+    </div>
+</div> */}
+
+
+// const parent=React.createElement(
+//     "div",{id:"parent"},
+//     React.createElement("div",{id:"child"},React.createElement("h1",{},"i am an h1 tag"))
+// );
+
+const parent=React.createElement(
+    "div",{id:"parent"},
+    React.createElement("div",{id:"child"},[
+        React.createElement("h1",{},"i am an h1 tag"),
+        React.createElement("p",{className:"para"},"passing more than i child by  using array")
+    ]
+    )
+    
+);
+
+console.log(parent);
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent); 
