98741076e05f47c923a668fa857f9dd2b5719726
manojkadali <160770025+manojkadali@users.noreply.github.com>
2025-12-28 23:24:07 +0530

    up to hooks

--- src/Components/__tests__/sum.test.js
+++
@@ -1,8 +0,0 @@
-import { Add } from "../Add";
-
-test("Sum function should calculate addition",()=>{
-   const result= Add(3,4);
-   
-   //Assertion
-   expect(result).toBe(7);
-})
--- src/utilis/constants.js
+++
@@ -1,5 +0,0 @@
-
-
-
-export const CON_URL="https://s3.amazonaws.com/cdn.designcrowd.com/blog/39-Food-Delivery-Logos-That-Will-Leave-You-Hungry-For-More/food-delivery-by-simplepixelsl-brandcrowd.png";
-
